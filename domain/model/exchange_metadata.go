2852d8385a1196342b8a9138a5a06d983364ff21
Yida Liu <yida.liu@qq.com>
2022-09-25 20:42:30 +0800

    [prototype 1] Added structure model

--- domain/model/exchange_metadata.go
+++ domain/model/exchange_metadata.go
@@ -1,19 +1,32 @@
 package model
 
+import "time"
+
+// ExchangeMetadata db type and internal model.
 type ExchangeMetadata struct {
-	Id           uint64
-	ExchangeName string
-	Extra        map[string]string
+	ID        uint   `gorm:"primarykey"`
+	Exchange  string `gorm:"uniqueIndex:uniq_exchange` // exchange name
+	Metadata  []byte // raw json string text column
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// TableName implements gorm.Tabler.
+func (ExchangeMetadata) TableName() string {
+	return "tab_orderbook_exchange_metadata"
 }
 
 type UpdateMetadataParam struct {
+	Metadata *ExchangeMetadata
 }
 
 type UpdateMetadataData struct {
 }
 
 type QueryMetadataParam struct {
+	ExchangeName string
 }
 
 type QueryMetadataData struct {
+	Metadata *ExchangeMetadata
 }
